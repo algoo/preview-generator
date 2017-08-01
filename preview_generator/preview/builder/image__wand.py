919501cf41f878335228f43fa1aa24728bfb4968
Damien Accorsi <damien@christophecolomb>
2017-08-01 18:41:09 +0200

    fixes black background when converting documetns to jpeg.

--- preview_generator/preview/builder/image__wand.py
+++ preview_generator/preview/builder/image__wand.py
@@ -17,11 +17,18 @@ def convert_pdf_to_jpeg(
         pdf: typing.Union[str, typing.IO[bytes]],
         preview_size: ImgDims
 ) -> BytesIO:
-    with WImage(file=pdf, background=Color('white')) as img:
+    with WImage(file=pdf) as img:
+        img.background_color = Color('white')
+        # HACK - D.A. - 2017-08-01
+        # The following line avoid black background in case of transparent
+        # objects found on the page. As we save to JPEG, this is not a problem
+        img.alpha_channel = False
+
         resize_dims = compute_resize_dims(
             ImgDims(img.width, img.height),
             preview_size
         )
+
         img.resize(resize_dims.width, resize_dims.height)
         content_as_bytes = img.make_blob('jpeg')
         output = BytesIO()
