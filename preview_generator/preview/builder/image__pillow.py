d772a89ebcb72c2885ec14f2cbf852240aeaa296
Guénaël Muller <guenael.muller@algoo.fr>
2019-06-18 16:35:26 +0200

    deal better with b&w image

--- preview_generator/preview/builder/image__pillow.py
+++ preview_generator/preview/builder/image__pillow.py
@@ -9,7 +9,7 @@ from preview_generator.preview.generic_preview import ImagePreviewBuilder
 from preview_generator.utils import compute_resize_dims
 from preview_generator.utils import ImgDims
 
-
+TRANSPARENCY_MODES = ['RGBA', 'LA']
 class ImagePreviewBuilderPillow(ImagePreviewBuilder):
     @classmethod
     def get_label(cls) -> str:
@@ -59,23 +59,43 @@ class ImagePreviewBuilderPillow(ImagePreviewBuilder):
                 dims_in=ImgDims(width=image.size[0], height=image.size[1]),
                 dims_out=preview_dims
             )
-
-            image = image.resize((resize_dim.width, resize_dim.height))
-            output_image = Image.new(
-                'RGB',
-                (resize_dim.width, resize_dim.height),
-                (255, 255, 255)
-            )
-
-            try:
-                output_image.paste(image, (0, 0), image)
-            except ValueError:
-                self.logger.warning(
-                    'Failed the transparency mask superposition. '
-                    'Maybe your image does not contain a transparency mask')
-                output_image.paste(image)
-
             output = BytesIO()
-            output_image.save(output, 'jpeg')
+            image = image.resize((resize_dim.width, resize_dim.height))
+            if image.mode not in TRANSPARENCY_MODES:
+                # INFO - G.M - 2019-06-18 - Try directly saving new image, this may failed due to
+                # image mode issue.
+                try:
+                    image.save(output, 'jpeg')
+                except OSError as exc:
+                    image = image.convert('RGB')
+                    image.save(output, 'jpeg')
+            else:
+                # INFO - G.M - 2019-06-18 - choose saving mode according to image mode, this should
+                # be jpeg compatible mode
+                if image.mode == 'RGBA':
+                    output_image = Image.new(
+                        'RGB',
+                        (resize_dim.width, resize_dim.height),
+                        (255, 255, 255)
+                    )
+                elif image.mode == 'LA':
+                    output_image = Image.new(
+                        'L',
+                        (resize_dim.width, resize_dim.height),
+                        1
+                    )
+                else:
+                    raise Exception()
+
+                # INFO - G.M - 2019-06-18 - in case of transparency mode, do apply image over blank output image.
+                try:
+                    output_image.paste(image, (0, 0), image)
+                except ValueError:
+                    self.logger.warning(
+                        'Failed the transparency mask superposition. '
+                        'Maybe your image does not contain a transparency mask')
+                    output_image.paste(image)
+                output_image.save(output, 'jpeg')
+
             output.seek(0, 0)
             return output
