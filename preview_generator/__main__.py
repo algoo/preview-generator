05f668377feb0e8d955643517da2e4d1e1312ffd
Julien Palard <julien@palard.fr>
2019-07-11 16:11:35 +0200

    Add as a console_script.

--- preview_generator/__main__.py
+++ preview_generator/__main__.py
@@ -1,16 +1,32 @@
+import argparse
+import sys
+
+from preview_generator.infos import __version__
 from preview_generator.utils import get_subclasses_recursively
 from preview_generator.preview.generic_preview import PreviewBuilder
 from preview_generator.manager import PreviewManager
 from preview_generator.exception import BuilderDependencyNotFound, ExecutableNotFound
-from preview_generator.preview.builder_factory import get_builder_folder_name, get_builder_modules, import_builder_module
-import argparse
+from preview_generator.preview.builder_factory import (
+    get_builder_folder_name,
+    get_builder_modules,
+    import_builder_module,
+)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Generates previews of files")
+    parser = argparse.ArgumentParser(
+        prog="preview_generator", description="Generates previews of files"
+    )
     parser.add_argument("input_files", nargs="*", help="File to preview")
     parser.add_argument("--check-dependencies", action="store_true")
-    return parser.parse_args()
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s " + __version__
+    )
+    args = parser.parse_args()
+    if not args.input_files and not args.check_dependencies:
+        parser.print_usage(file=sys.stderr)
+        exit(1)
+    return args
 
 
 def check_dependencies():
@@ -24,16 +40,16 @@ def check_dependencies():
         try:
             builder.check_dependencies()
         except (BuilderDependencyNotFound, ExecutableNotFound) as e:
-            print('Builder {} is missing a dependency: {}'.format(
-                builder.__name__,
-                e.__str__()
-            ))
-        except NotImplementedError:
-            self.logger.info(
-                'Skipping builder class [{}]: method get_supported_mimetypes '
-                'is not implemented'.format(builder.__name__)
+            print(
+                "Builder {} is missing a dependency: {}".format(
+                    builder.__name__, e.__str__()
+                )
             )
-
+        except NotImplementedError:
+            print(
+                "Skipping builder class [{}]: method get_supported_mimetypes "
+                "is not implemented".format(builder.__name__)
+            )
 
 
 def main():
