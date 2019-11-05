b82d46a68919fc33e5708f1926262a82f0c1af4d
Guénaël Muller <guenael.muller@algoo.fr>
2019-11-05 17:11:27 +0100

    simpler stl file

--- tests/input/stl/test_stl_vtk.py
+++ tests/input/stl/test_stl_vtk.py
@@ -13,9 +13,9 @@ from preview_generator.utils import ImgDims
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 CACHE_DIR = "/tmp/preview-generator-tests/cache/"
-# INFO - G.M - 2019-11-05 - image from https://www.thingiverse.com/thing:182337
-# under licence cc-by, created by yahoo Japan
-IMAGE_FILE_PATH = os.path.join(CURRENT_DIR, "Whale_t.stl")
+# INFO - G.M - 2019-11-05 - 3d object from https://www.thingiverse.com/thing:477
+# by bre under public domain
+IMAGE_FILE_PATH = os.path.join(CURRENT_DIR, "40mmcube.stl")
 
 
 def setup_function(function: typing.Callable) -> None:
@@ -28,7 +28,7 @@ def test_to_jpeg() -> None:
     builder = ImagePreviewBuilderVtk()
     assert builder.has_jpeg_preview() is True
     size = ImgDims(height=256, width=512)
-    preview_name = "stl_whale_test_vtk"
+    preview_name = "stl_cube_test_vtk"
     builder.build_jpeg_preview(
         file_path=IMAGE_FILE_PATH,
         size=size,
@@ -49,7 +49,7 @@ def test_to_jpeg() -> None:
 def test_get_nb_page() -> None:
     os.makedirs(CACHE_DIR)
     builder = ImagePreviewBuilderVtk()
-    preview_name = "stl_whale_test_vtk"
+    preview_name = "stl_cube_test_vtk"
     nb_page = builder.get_page_number(
         file_path=IMAGE_FILE_PATH, cache_path=CACHE_DIR, preview_name=preview_name
     )
