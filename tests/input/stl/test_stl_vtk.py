1df9f416d180adff7df1feea9296d9314e06ba31
Andrew Sweeney <asweeney86@gmail.com>
2021-05-19 10:32:11 -0400

    Update tests/input/stl/test_stl_vtk.py
    Co-authored-by: inkhey <mail@inkey-art.net>

--- tests/input/stl/test_stl_vtk.py
+++ tests/input/stl/test_stl_vtk.py
@@ -22,7 +22,7 @@ def setup_function(function: typing.Callable) -> None:
     shutil.rmtree(CACHE_DIR, ignore_errors=True)
 
 
-@pytest.mark.xfail(sys.version_info[:2] >= (3, 8), reason="vtk support for python 3.8 broken")
+@pytest.mark.xfail(sys.version_info[:2] >= (3, 8), reason="vtk support for python 3.8 and later is broken")
 def test_to_jpeg() -> None:
     os.makedirs(CACHE_DIR)
     builder = ImagePreviewBuilderVtk()
