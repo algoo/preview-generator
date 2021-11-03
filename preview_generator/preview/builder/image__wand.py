3aa7c7a07fbf3366c58cfad69695dcd5ccf78418
Guénaël Muller <guenael.muller@algoo.fr>
2021-11-03 09:23:48 +0100

    fix supported mimetype table and weight

--- preview_generator/preview/builder/image__wand.py
+++ preview_generator/preview/builder/image__wand.py
@@ -2,14 +2,14 @@
 import os
 import typing
 
+from wand.color import Color
 from wand.image import Image
 import wand.version
-from wand.color import Color
 
 from preview_generator.preview.generic_preview import ImagePreviewBuilder
 from preview_generator.utils import ImgDims
-from preview_generator.utils import imagemagick_supported_mimes
 from preview_generator.utils import compute_resize_dims
+from preview_generator.utils import imagemagick_supported_mimes
 
 DEFAULT_JPEG_QUALITY = 85
 DEFAULT_JPEG_PROGRESSIVE = True
@@ -17,13 +17,11 @@ DEFAULT_JPEG_PROGRESSIVE = True
 
 class ImagePreviewBuilderWand(ImagePreviewBuilder):
 
-    weight = 40
+    weight = 30
     MIMETYPES = []  # type: typing.List[str]
 
     def __init__(
-        self,
-        quality: int = DEFAULT_JPEG_QUALITY,
-        progressive: bool = DEFAULT_JPEG_PROGRESSIVE,
+        self, quality: int = DEFAULT_JPEG_QUALITY, progressive: bool = DEFAULT_JPEG_PROGRESSIVE,
     ):
         super().__init__()
         self.quality = quality
@@ -75,12 +73,7 @@ class ImagePreviewBuilderWand(ImagePreviewBuilder):
         dest_path = os.path.join(cache_path, preview_name)
         self.image_to_jpeg_wand(file_path, size, dest_path)
 
-    def image_to_jpeg_wand(
-        self,
-        file_path: str,
-        preview_dims: ImgDims,
-        dest_path: str,
-    ) -> None:
+    def image_to_jpeg_wand(self, file_path: str, preview_dims: ImgDims, dest_path: str,) -> None:
         with Image(filename=file_path) as img:
             # https://legacy.imagemagick.org/Usage/thumbnails/
             img.auto_orient()
